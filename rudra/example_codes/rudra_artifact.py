ed9b0a3aa6b5b5e5d5e300e131e0668faa5c7094
shivaprasad ramachandrappa <s.ramachandrappa@sage.com>
2024-11-10 00:09:33 +0000

    creating a code file for rudra

---
+++ rudra/example_codes/rudra_artifact.py
@@ -0,0 +1,24 @@
+import turtle
+
+# Initialize Turtle
+screen = turtle.Screen()
+screen.bgcolor("white")
+artist = turtle.Turtle()
+artist.speed(10)
+
+# Define colors and variables
+#colors = ["red", "blue", "green", "purple", "orange"]
+
+angle = 36  # Angle to rotate after each circle
+radius = 50
+
+# Draw spiral flower
+artist.begin_fill()
+for i in range(36):  # Loop to draw 36 circles in a pattern
+    #artist.color(colors[i % len(colors)])  # Cycle through colors
+    artist.circle(radius)  # Draw a circle
+    artist.right(angle)  # Rotate turtle
+    radius += 2  # Increase the radius gradually
+    artist.fillcolor("red")
+artist.end_fill()
+turtle.done()
